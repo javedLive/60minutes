79cec7f6285b5627df24c1a89b5e1ca756066c28
Nazmul Hussain <jnazmul@student.nitw.ac.in>
2018-05-08 13:52:23 +0600

    First Commit

---
+++ 60minutes/src/components/Home.vue
@@ -0,0 +1,46 @@
+<template>
+	<div>
+		<h1>Hello World</h1>
+		<form @submit.prevent="add">
+			<input type="text" v-model="newItem" name="">
+			<button type="submit">Add</button>
+		</form>
+		
+		<items :items="lists"></items>
+	</div>
+</template>
+<script type="text/javascript">
+import items from './Items';
+	export default{
+
+		components : {items},
+
+		data(){
+			return {
+				lists : [
+					{'name' : 'Javed'},
+					{'name': 'Hossain'},
+					{'name' : 'Ahsan'},
+					{'name' : 'Babul'},
+				],
+				newItem : null
+			}
+		},
+		methods:{
+			add(){
+				this.lists.push({'name':this.newItem})
+				this.newItem = null
+			}
+		},
+			created(){
+				EventBus.$on('itemRemove',(index)=>{
+					this.lists.splice(index,1)
+				})
+				
+			 }
+		
+	}
+</script>
+<style type="text/css">
+	
+</style>
---
+++ 60minutes/src/components/Items.vue
@@ -0,0 +1,26 @@
+<template>
+<ul>
+  <li 
+      v-for="(item,index) in items" 
+      :key="item.name"
+      v-if="item.name !== 'Nazmul' ">
+    	{{item.name}} 
+    <button @click="remove(index)">X</button>
+  </li>
+  	<li v-else> No Nazmul Allow Here</li>
+</ul>
+</template>
+<script type="text/javascript">
+	export default{
+		props:['items'],
+		methods:{
+			remove(index){
+				EventBus.$emit('itemRemove',index)
+			}
+		}
+
+	}
+</script>
+<style type="text/css">
+	
+</style>
